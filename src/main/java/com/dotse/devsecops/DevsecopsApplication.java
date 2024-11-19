894aa6019ab05e49d19328e81b63954c5b9afae9
Benjamin. <dotsehyde@gmail.com>
2024-11-19 15:58:58 +0000

    Web app commit

---
+++ src/main/java/com/dotse/devsecops/DevsecopsApplication.java
@@ -0,0 +1,21 @@
+package com.dotse.devsecops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevsecopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevsecopsApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "My Awesome Project.";
+    }
+
+}
